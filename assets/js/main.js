f62be26ffe181dd44fee04ce3035fc73cabc9ea8
webled <spoovi@mail.ru>
2020-11-24 15:20:15 +0300

    gdf

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,11 @@
 butt.onclick = function () {
   var val = document.getElementById("elem1").value;
 
+  var telegram =
+    "https://api.telegram.org/bot1403106172:AAF0cYqbdUgpDOQqllWvZTo1vOfUUNoDvo4/sendMessage?chat_id=-1001077685352&text=";
+
   var getPageList = new XMLHttpRequest();
-  getPageList.open(
-    "POST",
-    "https://api.telegram.org/bot1403106172:AAF0cYqbdUgpDOQqllWvZTo1vOfUUNoDvo4/sendMessage?chat_id=-1001077685352&text=" +
-      val,
-    true
-  );
+  getPageList.open("POST", telegram + val, true);
 
   getPageList.send(null);
 };
