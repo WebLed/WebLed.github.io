9863f4abb4628555f3252814a8a846f3cc4848fa
webled <spoovi@mail.ru>
2020-11-24 15:09:33 +0300

    masseg

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,12 @@
 butt.onclick = function () {
   var val = document.getElementById("elem1").value;
-  window.open(
-    "https://api.telegram.org/bot1403106172:AAF0cYqbdUgpDOQqllWvZTo1vOfUUNoDvo4/sendMessage?chat_id=-1001077685352&parse_mode=html&text=" +
-      val
+
+  var getPageList = new XMLHttpRequest();
+  getPageList.open(
+    "POST",
+    "https://api.telegram.org/bot1403106172:AAF0cYqbdUgpDOQqllWvZTo1vOfUUNoDvo4/sendMessage?chat_id=-1001077685352&text=ffff",
+    true
   );
-  window.close();
+
+  getPageList.send(null);
 };
